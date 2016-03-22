f76812d6bca787b4c1d212c83d6bef3bc6633324
Kristian Segnana <kristian.segnana@gmail.com>
2016-03-22 19:01:24 +0100

    thfh

---
+++ src/introsde/document/soap/ControlGoal.java
@@ -0,0 +1,60 @@
+
+package introsde.document.soap;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java per controlGoal complex type.
+ * 
+ * <p>Il seguente frammento di schema specifica il contenuto previsto contenuto in questa classe.
+ * 
+ * <pre>
+ * &lt;complexType name="controlGoal">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="type" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "controlGoal", propOrder = {
+    "type"
+})
+public class ControlGoal {
+
+    protected String type;
+
+    /**
+     * Recupera il valore della proprietà type.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Imposta il valore della proprietà type.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setType(String value) {
+        this.type = value;
+    }
+
+}
---
+++ src/introsde/document/soap/CreateFoodResponse.java
@@ -0,0 +1,62 @@
+
+package introsde.document.soap;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java per createFoodResponse complex type.
+ * 
+ * <p>Il seguente frammento di schema specifica il contenuto previsto contenuto in questa classe.
+ * 
+ * <pre>
+ * &lt;complexType name="createFoodResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element ref="{http://soap.document.introsde/}food" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "createFoodResponse", propOrder = {
+    "food"
+})
+public class CreateFoodResponse {
+
+    @XmlElement(namespace = "http://soap.document.introsde/")
+    protected Food food;
+
+    /**
+     * Recupera il valore della proprietà food.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Food }
+     *     
+     */
+    public Food getFood() {
+        return food;
+    }
+
+    /**
+     * Imposta il valore della proprietà food.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Food }
+     *     
+     */
+    public void setFood(Food value) {
+        this.food = value;
+    }
+
+}
